e14bfdb389f58f56c4ca1ade07db057d871ea716
Lee Thomas <number8pie@gmail.com>
2016-03-15 15:35:30 +0000

    Fixed a mis-typed value in the generate_sort_links() switch. Line 91 was "sort=3" and should have been "sort=5".

--- search.php
+++ search.php
@@ -88,7 +88,7 @@
       case 3:
         $sort_links .= '<td><a href="' . $_SERVER['PHP_SELF'] . '?usersearch=' . $user_search . '&sort=1">Job Title</a></td><td>Description</td>';
         $sort_links .= '<td><a href="' . $_SERVER['PHP_SELF'] . '?usersearch=' . $user_search . '&sort=4">State</a></td>';
-        $sort_links .= '<td><a href="' . $_SERVER['PHP_SELF'] . '?usersearch=' . $user_search . '&sort=3">Date Posted</a></td>';
+        $sort_links .= '<td><a href="' . $_SERVER['PHP_SELF'] . '?usersearch=' . $user_search . '&sort=5">Date Posted</a></td>';
         break;
       case 5:
         $sort_links .= '<td><a href="' . $_SERVER['PHP_SELF'] . '?usersearch=' . $user_search . '&sort=1">Job Title</a></td><td>Description</td>';
