65f5542687995cde688c9481403b26eb9ea26495
Lee Thomas <number8pie@gmail.com>
2016-03-15 11:14:09 +0000

    Created a function that builds heading links on the specified sort setting, function uses a switch.

--- search.php
+++ search.php
@@ -12,29 +12,13 @@
   <h3>Risky Jobs - Search Results</h3>
 
 <?php
-  // Grab the sort setting and search keywords from the URL using GET
-  $sort = $_GET['sort'];
-  $user_search =  $_GET['usersearch'];
-
-  // Start generating the table of results
-  echo '<table border="0" cellpadding="2">';
-
-  // Generate the search result headings
-  echo '<tr class="heading">';
-  echo '<td>Job Title</td><td>Description</td><td>State</td><td>Date Posted</td>';
-  echo '</tr>';
-
-  // Connect to the database
-  require_once('connectvars.php');
-  $dbc = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
-
+  // This function builds a search query from the search keywords and sort setting
   function build_query($user_search) {
     $search_query = "SELECT * FROM riskyjobs";
 
     // Extract the search keywords into an array
     $clean_search = str_replace(',', ' ', $user_search);
     $search_words = explode(' ', $clean_search);
-
     $final_search_words = array();
     if (count($search_words) > 0) {
       foreach ($search_words as $word) {
@@ -60,6 +44,52 @@
 
     return $search_query;
   }
+
+  // This function builds heading links based on the specified sort setting
+  function generate_sort_links($user_search, $sort) {
+    $sort_links = '';
+
+    switch ($sort) {
+      case 1:
+        $sort_links .= '<td><a href="' . $_SERVER['PHP_SELF'] . '?usersearch=' . $user_search . '&sort=2">Job Title</a></td><td>Description</td>';
+        $sort_links .= '<td><a href="' . $_SERVER['PHP_SELF'] . '?usersearch=' . $user_search . '&sort=3">State</a></td>';
+        $sort_links .= '<td><a href="' . $_SERVER['PHP_SELF'] . '?usersearch=' . $user_search . '&sort=5">Date Posted</a></td>';
+        break;
+      case 3:
+        $sort_links .= '<td><a href="' . $_SERVER['PHP_SELF'] . '?usersearch=' . $user_search . '&sort=1">Job Title</a></td><td>Description</td>';
+        $sort_links .= '<td><a href="' . $_SERVER['PHP_SELF'] . '?usersearch=' . $user_search . '&sort=4">State</a></td>';
+        $sort_links .= '<td><a href="' . $_SERVER['PHP_SELF'] . '?usersearch=' . $user_search . '&sort=3">Date Posted</a></td>';
+        break;
+      case 5:
+        $sort_links .= '<td><a href="' . $_SERVER['PHP_SELF'] . '?usersearch=' . $user_search . '&sort=1">Job Title</a></td><td>Description</td>';
+        $sort_links .= '<td><a href="' . $_SERVER['PHP_SELF'] . '?usersearch=' . $user_search . '&sort=3">State</a></td>';
+        $sort_links .= '<td><a href="' . $_SERVER['PHP_SELF'] . '?usersearch=' . $user_search . '&sort=6">Date Posted</a></td>';
+        break;
+      default:
+        $sort_links .= '<td><a href="' . $_SERVER['PHP_SELF'] . '?usersearch=' . $user_search . '&sort=1">Job Title</a></td><td>Description</td>';
+        $sort_links .= '<td><a href="' . $_SERVER['PHP_SELF'] . '?usersearch=' . $user_search . '&sort=3">State</a></td>';
+        $sort_links .= '<td><a href="' . $_SERVER['PHP_SELF'] . '?usersearch=' . $user_search . '&sort=5">Date Posted</a></td>';
+        break;
+    }
+    
+    return $sort_links;
+  }
+
+  // Grab the sort setting and search keywords from the URL using GET
+  $sort = $_GET['sort'];
+  $user_search =  $_GET['usersearch'];
+
+  // Start generating the table of results
+  echo '<table border="0" cellpadding="2">';
+
+  // Generate the search result headings
+  echo '<tr class="heading">';
+  //echo '<td>Job Title</td><td>Description</td><td>State</td><td>Date Posted</td>';
+  echo '</tr>';
+
+  // Connect to the database
+  require_once('connectvars.php');
+  $dbc = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
 
   $search_query = build_query($user_search);
 
